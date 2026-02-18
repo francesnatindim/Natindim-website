d128615b6e37eadecc03b71548cb03d0664c956c
francesnatindim <frances.natindim@lccdo.edu.ph>
2026-02-18 16:25:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const btn = document .getElementById("button");
 let header1 = document.getElementById("header1");
-
+let image1 = document.getElementById("image1");
 
 btn.addEventListener("click", function() {
-    header1.textContent = " Paminawin mo ang music hihihi!";
-
-
+    header1.textContent = " chill and listen ya nyhahah";
+image1.src = "./images.jfif"
 });
-
